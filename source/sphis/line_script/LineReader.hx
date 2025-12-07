3d390510f8d8ccc23e951dcd2a2aeaa914e4d22c
Viwu <sinconsistencia@gmail.com>
2025-12-07 13:31:20 -0600

    meh, its fine

--- source/sphis/line_script/LineReader.hx
+++ source/sphis/line_script/LineReader.hx
@@ -5,22 +5,25 @@ import lime.app.Application;
 class LineReader
 {
 	public static function read(line:String, script:LineScript)
-    {
-        var split:Array<String> = line.split(' : ');
+	{
+		var split:Array<String> = line.split(' : ');
 
-        switch(split[0].toLowerCase())
-        {
-            case 'trace':
-                split.remove(split[0]);
-                
-                var the_trace:String = '';
-                for (bit in split)
-                    the_trace += bit + ' ';
+		switch (split[0].toLowerCase())
+		{
+			case 'trace':
+				split.remove(split[0]);
 
-                trace(script.file_path + ' : ' + the_trace, null);
+				var the_trace:String = '';
+				for (bit in split)
+					the_trace += bit + ' ';
 
-            default:
-                Application.current.window.alert('ULB: ' + split[0], 'Unknown Line Bit');
-        }
-    }
+				trace(script.file_path + ' : ' + the_trace, null);
+
+			case '':
+				// empty
+
+			default:
+				Application.current.window.alert('ULB: ' + split[0], 'Unknown Line Bit');
+		}
+	}
 }
