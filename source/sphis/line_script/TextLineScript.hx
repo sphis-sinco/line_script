1e5aaad935f0ccc3bea003617fe5bd41e8fe51ca
Viwu <sinconsistencia@gmail.com>
2025-12-07 13:21:58 -0600

    text line script

---
+++ source/sphis/line_script/TextLineScript.hx
@@ -0,0 +1,41 @@
+package sphis.line_script;
+
+import lime.utils.Assets;
+
+class TextLineScript extends LineScript
+{
+	override public function new(file_name:String, file_path:String)
+	{
+		super(file_name, file_path);
+	}
+
+	override function load()
+	{
+		var read:String->String = file -> {
+			#if sys
+			return sys.io.File.getContent(file);
+			#else
+			return Assets.getText(file);
+			#end
+		};
+
+		lines = read(file_path).split('\n');
+
+		super.load();
+	}
+
+	override function save()
+	{
+		super.save();
+
+		#if sys
+        var file_content:String = '';
+        for (line in lines)
+            file_content += line + '\n';
+        
+		sys.io.File.saveContent(file_path, file_content);
+		#else
+		trace('Cannot save');
+		#end
+	}
+}
